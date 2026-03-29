4deb13229fd9055218eec9d2d8575a029354a3d5
ssandt4 <shani@lastcarproductions.com>
2026-03-28 22:35:34 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
-const express = require('express');
+cookie: {
+        maxAge: 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        secure: false,  // Changed from process.env.NODE_ENV === 'production'
+        sameSite: 'lax'  // Added thisconst express = require('express');
 const path = require('path');
 const session = require('express-session');
 const app = express();
